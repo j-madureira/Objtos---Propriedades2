7ebaddc93a5d3ace486f4dfa8fe155ce7b1e062a
João Vitor Madureira <103207658+j-madureira@users.noreply.github.com>
2022-04-29 14:32:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var pessoa1 = {nome:"Jonas",cpf:"1881212122",rg:"564564654"};
+var pessoa2 = {nome:"João",cpf:"883128328",rg:"9932193323"};
+
+//Pessoa1
+console.log(`Nome: ${pessoa1["nome"]}`);
+console.log(`CPF: ${pessoa1.cpf}`);
+console.log(`RG: ${pessoa1.rg}`);
+
+//Pessoa2
+console.log(`Nome: ${pessoa2.nome}`);
+console.log(`CPF: ${pessoa2.cpf}`);
+console.log(`RG: ${pessoa2.rg}`);
+
